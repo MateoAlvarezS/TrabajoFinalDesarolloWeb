ec6c63058e9da4802d81ded587405832a8446e71
MateoAlvarezS <MateoAlvarezS>
2022-10-30 14:30:54 -0500

    Barra de navegacion añadida

---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,19 @@
+import styles from "../../styles/Navbar.module.css";
+import logo from "../../assets/logo.svg";
+
+function Navbar() {
+  return (
+    <nav className={styles["nav"]}>
+      <div className={styles['nav-logo']}>
+        <img src={logo} alt="logo" />
+        <p>Productos</p>
+      </div>
+      <button className={styles['nav-cart']}>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <p>Mi Carrito</p>
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
